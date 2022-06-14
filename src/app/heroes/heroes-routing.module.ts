8b023e38228869d4e9dec4905de262f5170cb869
Ignacio Chirino <ignacio.chirino@alumnos.ucn.cl>
2022-06-14 00:41:08 -0400

    feat(app/heroes): implementamos lazy load de forma adecuada.

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,44 +1,46 @@
 import { NgModule } from '@angular/core';
-import { ListadoComponent } from './pages/listado/listado.component'
-import { AgregarComponent } from './pages/agregar/agregar.component'
-import { BuscarComponent } from './pages/buscar/buscar.component'
-import { VerComponent } from './pages/ver/ver.component'
+import { ListadoComponent } from './pages/listado/listado.component';
+import { AgregarComponent } from './pages/agregar/agregar.component';
+import { BuscarComponent } from './pages/buscar/buscar.component';
+import { VerComponent } from './pages/ver/ver.component';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: HomeComponent,
     children: [
       {
-       path: 'listado',
-       component: ListadoComponent
+        path: 'listado',
+        component: ListadoComponent,
       },
       {
         path: 'agregar',
-        component: AgregarComponent
-       },
-       {
+        component: AgregarComponent,
+      },
+      {
         path: 'editar/:id',
-        component: AgregarComponent
-       },
-       {
+        component: AgregarComponent,
+      },
+      {
         path: 'buscar',
-        component: BuscarComponent
-       },
-       {
+        component: BuscarComponent,
+      },
+      {
         path: ':id',
-        component: VerComponent
-       },
-       {
+        component: VerComponent,
+      },
+      {
         path: '**',
-        redirectTo: 'listado'
-       },
-    ]
-  }
-]
+        redirectTo: 'listado',
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class HeroesRoutingModule { }
+export class HeroesRoutingModule {}
